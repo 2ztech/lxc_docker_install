76828da44f9da52de8434db43feeea7159a49d44
lorimaju <115074222+lorimaju@users.noreply.github.com>
2023-02-14 04:13:53 +0800

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -5,10 +5,7 @@
   
   #portainer ce install
   docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-  
-  #test installation
-  sudo docker run hello-world
-  
+    
   #message
   ip=$(hostname -I | awk '{print $1}')
   port="9443"
