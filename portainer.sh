e49da31cb12337b34216ab3c0e5c45a2bb7b7a7c
lorimaju <115074222+lorimaju@users.noreply.github.com>
2023-02-14 04:02:27 +0800

    Create portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+ #portainer volume create
+ docker volume create portainer_data
+  
+  #portainer ce install
+  docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+  
+  #test installation
+  sudo docker run hello-world
+  
+  #message
+  ip=$(hostname -I | awk '{print $1}')
+  port="9443"
+
+  printf "You can access Portainer at https://%s:%s\n" "$ip" "$port"
